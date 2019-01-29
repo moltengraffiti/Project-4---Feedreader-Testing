91995b0fb610bd39c71cd4e7df2d665649e6774e
Sam Reilly <samreilly@gmail.com>
2019-01-29 13:43:53 +1100

    TODO2 - Feeds have name >0

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,7 +31,7 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-        it('has url', function(){
+        it('All feeds have a url, url is not empty', function(){
             for (let feed of allFeeds){
                 expect(feed.url).toBeDefined();
                 expect(feed.url).not.toBe(0);
@@ -42,6 +42,14 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+
+         it('All feeds have a name, name is not empty', function(){
+            for(let feed of allFeeds){
+                expect(feed.name).toBeDefined();
+                expect(feed.name).not.toBe(0); 
+            }
+
+         });
     });
 
 
